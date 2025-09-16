7a66b68f11111b4647fbe32efb3bb802f6bdc527
ktiays <cuansky@gmail.com>
2025-09-16 21:24:13 +0800

    Update swift-syntax dependency to version 602.0.0

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "602.0.0"),
     ],
     targets: [
         .macro(
